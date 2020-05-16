70c05d80d9ccf19283d1a453db28b943e46a6f18
nextdoorwith <nextdoorwithadm@gmail.com>
2020-05-16 20:49:44 +0900

    update comment and so on.

--- basic-example/HttpClientSimple/Controllers/ApiTestController.cs
+++ basic-example/HttpClientSimple/Controllers/ApiTestController.cs
@@ -5,8 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace HttpClientSimple.Controllers
 {
     [Route("api/[controller]")]
