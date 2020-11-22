46988ef610195c14cb76ce875dc3ebe3f565e7e1
nextdoorwith <nextdoorwithadm@gmail.com>
2020-11-22 22:27:20 +0900

    add example for reverse proxy

---
+++ basic-example/ReverseProxyExample/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ReverseProxyExample.Models;
+using ReverseProxyExample.Models.Home;
+
+namespace ReverseProxyExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(HomeViewModel model)
+        {
+            var dic = model.Headers;
+            dic.Clear();
+            foreach(var item in Request.Headers.Keys)
+            {
+                dic[item] = Request.Headers[item];
+            }
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ basic-example/ReverseProxyExample/Models/Home/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReverseProxyExample.Models.Home
+{
+    public class HomeViewModel
+    {
+        public Dictionary<string, string> Headers = new Dictionary<string, string>();
+    }
+}
